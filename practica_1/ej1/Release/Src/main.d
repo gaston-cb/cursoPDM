c64577b2938e40ad2da4d3df915e628ecab8b631
Gaston Valdez <gaston_15_cb@hotmail.com>
2022-03-12 13:32:13 -0300

    first commit

---
+++ practica_1/ej1/Release/Src/main.d
@@ -0,0 +1,84 @@
+Src/main.o: ../Src/main.c ../Inc/main.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/BSP/STM32F4xx_Nucleo_144/stm32f4xx_nucleo_144.h
+
+../Inc/main.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+../Inc/stm32f4xx_hal_conf.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+D:/curso_sistemas_embebidos/programacionMicroprocesadores/practica_1/ej1/Drivers/BSP/STM32F4xx_Nucleo_144/stm32f4xx_nucleo_144.h:
