0e5b2f9f47f4ef18e3908fa3099ff250afa0176d
Gaston Valdez <gaston_15_cb@hotmail.com>
2022-03-12 19:50:04 -0300

    se agregaron comentarios al código y mejora del readme.md

--- practica_1/ej1/Src/main.c
+++ practica_1/ej1/Src/main.c
@@ -129,21 +129,30 @@ int main(void)
 void delayInit( delay_t * delay, tick_t duration )
 {
 
-	if (delay == NULL){
+	// si no hay puntero, o la duración es cero, no configura el clock
+	if (delay == NULL || duration == 0){
 		return ;
 	}
 	delay->duration = duration ;
 
 }
 
+
+/*
+ * delay_t campo running false-> lo cambia a true, y toma el valor del system tick.En este caos
+ * 								la rta es false, porque recien inicia
+ * delay_t campo running true -> realiza la cuenta system_tick - startTime: si es mayor o igual al campo duration, responde true
+ * 								en caso contrario false
+ * */
 bool_t delayRead( delay_t * delay )
 {
-	bool_t response = false ; // ver a que nombre cambiar para mejorar legilibiladad
-	// como recien inicia, entonces la respuesta es false
-	if (delay == NULL){ // aca estaria bueno encender errores de compilación
+	bool_t response = false ; // respuesta de la función readDelay
+	// no se considera el caso de duration= 0 , porque no podría ocurrir
+	if (delay == NULL){
 		return response ;
 	}
 	// si el puntero no es nulo
+	// como recien inicia, entonces la respuesta es false
 	if (delay->running == false){
 		delay->startTime = (tick_t )HAL_GetTick();
 		delay->running = true ;
@@ -161,8 +170,17 @@ bool_t delayRead( delay_t * delay )
 
 }
 
+/*
+ * tick_t duration: cambia la duración del retardo
+ * delay_t delay: clock dek sistema que se debe cambiar la duración */
 void delayWrite( delay_t * delay, tick_t duration )
 {
+
+	// si la duración es cero o no hay puntero asignado, retorna la función
+
+	if (delay_t == NULL || duration ==  0){
+		return ;
+	}
 	if (delay->duration != duration){
 		delay->duration = duration ;
 	}
