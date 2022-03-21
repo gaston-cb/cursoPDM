35ebb2a4f97b6ca6d26d9f929be97be8fec5d556
Gaston Valdez <gaston_15_cb@hotmail.com>
2022-03-21 19:48:18 -0300

    comentarios adicionados en formato de 'doxygen'

--- practica_3/ej1/Drivers/API/inc/API_delay.h
+++ practica_3/ej1/Drivers/API/inc/API_delay.h
@@ -9,6 +9,11 @@
 #include <stdint.h>
 typedef uint32_t tick_t;
 typedef bool bool_t;
+/**
+ * @brief Estructura de datos para manejo de retardos no bloqueantes, o
+ * 		  control de tiempos.
+ *
+ */
 typedef struct{
    tick_t startTime;
    tick_t duration;
@@ -16,24 +21,36 @@ typedef struct{
 } delay_t;
 
 /**
- * @brief
+ * @brief Configura un tiempo de duración del tiempo para un clock del sistema pero
+ * 		  no lo enciende, es decir, no lo pone a correr.
  *
- * @param delay
- * @param duration
+ * @param delay puntero a un clock del sistema de retardos no bloqueantes
+ * @param duration: duración del retardo en este sistema.
  */
 void delayInit( delay_t * delay, tick_t duration );
+
+
 /**
- * @brief
+ * @brief si el campo running es false lo cambia a true
+ * .En este caso la rta es false, porque recien inicia
  *
- * @param delay
- * @return valor booleano
+ * @param delay: puntero a estructura del tipo delay_t, definida para encapsular
+ * 		  el tiempo de uso de un retardo no bloqueante.
+ * @return  si el campo running es false lo cambia a true .En este caso la rta es false, porque recien inicia
+ *			si el campo running es true -> realiza la cuenta system_tick - startTime: si es mayor o igual al campo duration, responde true
+ * 			en caso contrario false
  */
 bool_t delayRead( delay_t * delay );
+
+
+
+
 /**
- * @brief
+ * @brief toma un puntero al clock, y le cambia la duración, en caso que sea distina
+ * 		  de la que tiene, o sea un puntero nulo.
  *
- * @param delay
- * @param duration
+ * @param delay clock del sistema que se debe cambiar la duracuion
+ * @param duration: cambia la duraciòn del retardo
  */
 void delayWrite( delay_t * delay, tick_t duration );
 
